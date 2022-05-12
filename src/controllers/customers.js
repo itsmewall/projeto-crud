dcd1b2dcf295c84f06c2d26045a0e574bb6b1dc2
Wallace Oliveira <wallacedeoliveira10@hotmail.com>
2022-05-12 12:15:49 -0300

    add remover clientes

--- src/controllers/customers.js
+++ src/controllers/customers.js
@@ -79,10 +79,21 @@ async function edit(req, res) {
 
 }
 
+async function remove(req, res) {
+    const { id } = req.params
+
+    const remove = await CustomersModel.deleteOne({ _id: id })
+
+    if (remove.ok) {
+        res.redirect('/list')
+    }
+}
+
 module.exports = {
     index,
     add,
     list,
     formEdit,
     edit,
+    remove,
 }
